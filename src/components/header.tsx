942f2687c628a24fa9dbddf1b12fd3bd4fe2ebfd
yukkooooo <calm.and.clear.02@gmail.com>
2024-08-03 06:02:44 +0900

    変更

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,20 +7,20 @@ import { ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 const Header: React.FC = () => {
   return (
-    <header className="flex position:fixd">
-      <div className="container mx-auto px-5 py-2 flex items-center">
-        <ul>
+    <header className="flex justify-between items-center  w-full px-7 fixed top-0 left-0  bg-white shadow-md z-50">
+      <div className=" flex px-5 py-1 justify-between items-center w-full mt-13">
+        <ul className='flex  justify-between '>
           <li className="group relative inline-flex h-9 items-center justify-center overflow-hidden rounded-full bg-emerald-950 px-4 font-medium text-neutral-200" ><Link href="/">login</Link></li>
         </ul>
 
-        <nav className=" flex justify-between items-center mx-auto px-5 py-2  ">
-          <ul className="w-10 flex " >
+        <nav className=" flex justify-between items-center px-5 py-2 w-full ">
+          <ul className="flex  space-x-4" >
             <li className=" mx-auto px-2 "><Link href="/">home</Link></li>
             <li className=" mx-auto px-2 "><Link href="/">about</Link></li>
             <li className=" mx-auto px-2 "><Link href="/">contact</Link></li>
 
           </ul>
-          <ul className="w-10 flex ">
+          <ul className="flex space-x-4 ">
             <li className=" mx-auto px-2 "><Link href="/about"> <HeartIcon className="w-5 h-5" /></Link></li>
             <li className=" mx-auto px-2 "><Link href="/about"><ShoppingCartIcon className="w-5 h-5" /></Link></li>
           </ul>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link.js";
 import React, { ReactNode } from 'react';
 import BasicSlider from '../components/BasicSlider';
 import Button from '../components/Button';
-import Image from 'next/image';
+
 
 // ブログデータの型を定義
 interface Blog {
@@ -47,9 +47,9 @@ export const getStaticProps: GetStaticProps = async () => {
   const categoryData = await client.get({ endpoint: "categories" });
   // console.log(categoryData);
   const blogData = await client.get({ endpoint: "blog" });
-  // console.log(blogData);
+  console.log(blogData);
   const eventData = await client.get({ endpoint: "event" });
-  console.log(eventData)
+  // console.log(eventData)
 
 
 
@@ -68,21 +68,21 @@ const Home: React.FC<HomeProps> = ({ blog, categories, event }) => {
 
   return (
     <div>
-      <div>
+      <div >
         <BasicSlider />
       </div>
-      <h4 className="flex items-center justify-center mt-10 mb-6 text-xl ">
+      <h4 className="flex items-center justify-center mt-10 text-xl p-10 ">
         CATEGORY
       </h4>
-      <div className="flex items-center justify-center md:flex-row">
-        <article className="flex items-center justify-center md:flex-row">
+      <div className="flex items-center justify-center flex-wrap ">
+        <article className="flex items-center justify-center md:flex-row flex-wrap ">
           {categories.map((category) => (
             <button
               type="button"
-              className="m-6 mt-2 inline-block rounded-full border-2 border-primary-100 px-10 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary-700 shadow-lg transition duration-150 ease-in-out hover:border-primary-accent-200 hover:bg-secondary-50/50 focus:border-primary-accent-200 focus:bg-secondary-50/50 focus:outline-none focus:ring-0 active:border-primary-accent-200 motion-reduce:transition-none dark:border-primary-400 dark:text-primary-300 dark:hover:bg-blue-950 dark:focus:bg-blue-950"
+              className="flex justify-center items-center flex-wrap md:flex-row m-6 mt-2 rounded-full border-2 border-primary-100 px-10 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary-700 shadow-lg transition duration-150 ease-in-out hover:border-primary-accent-200 hover:bg-secondary-50/50 focus:border-primary-accent-200 focus:bg-secondary-50/50 focus:outline-none focus:ring-0 active:border-primary-accent-200 motion-reduce:transition-none dark:border-primary-400 dark:text-primary-300 dark:hover:bg-blue-950 dark:focus:bg-blue-950"
               key={category.id}
             >
-              <Link href={`/category/${categories.id}`}>
+              <Link href={`category/${category.id}`}>
                 <p>{category.name}</p>
               </Link>
             </button>
@@ -91,45 +91,50 @@ const Home: React.FC<HomeProps> = ({ blog, categories, event }) => {
       </div>
 
       <div>
-        <h4 className="flex items-center justify-center m-10 text-xl">NEW ARRIVALS</h4>
-        <div>
-          <article className="flex flex-wrap justify-center">
+        <h4 className="flex items-center justify-center m-5 text-xl">NEW ARRIVALS</h4>
+
+        <div className="flex flex-wrap justify-center">
+          <article className="flex flex-wrap justify-center m-5">
             {blog.map((blog) => (
-              <div key={blog.id} className="flex justify-center items-center flex-wrap md:flex-row">
+              <div key={blog.id} className="mx-auto text-center ">
                 <Link href={`blog/${blog.id}`}>
-                  <Image
+                  <img
                     src={blog.item_image.url}
                     alt={blog.title}
                     width={blog.item_image.width}
                     height={blog.item_image.height}
-                    className="object-cover md:w-[70%] lg:w-[40%] mx-auto"
+                    className="w-[180px] h-[180px] object-cover first-letter:mx-auto  shadow-lg border-1 border-gray-300 rounded-xl mx-5"
                   />
                 </Link>
+                <h4 className="m-6 inline-block size-100">{blog.item_item_name}{blog.item_price_tax}円</h4>
+
               </div>
             ))}
           </article>
+
+
         </div>
 
-        <div className="flex items-center justify-center m-10">
-          <Button onClick={() => SubmitEvent}>MORE</Button>
-        </div>
-        <div>
+
+
+
+        <div className="w-4/5 mx-auto">
           <h4 className="flex items-center justify-center m-10 text-xl">EVENT</h4>
 
-          <article className="flex flex-wrap justify-center m-4.2">
+          <article className="flex flex-wrap justify-center">
             {event.map((event: any) => (
-              <div key={event.id} className="p-6.2 md:w-1/2 lg:w-1/4 flex justify-center items-center">
+              <div key={event.id} className="p-3.2 md:w-1/2 lg:w-1/4 flex justify-center items-center">
 
-                <div className="text-center">
+                <div className="text-center m-10">
 
 
                   <Link href={`event/${event.id}`}>
-                    <Image
+                    <img
                       src={event.eyecatch_event.url}
                       alt={event.eyecatch_event}
-                      width={150}
-                      height={150}
-                      className="w-[300px] h-[300px] mx-auto object-cover shadow-lg border-2 border-gray-300 rounded-xl"
+                      width={100}
+                      height={100}
+                      className="w-[235px] h-[235px] mx-auto object-cover shadow-lg border-2 border-gray-300 rounded-xl"
                     />
                   </Link>
 
